7811a3633427fc797ce33d40ecb6f73aeff4fca1
Mustafa <Mustafa@139.179.32.89>
2011-12-15 21:27:21 +0200

    First commit.

---
+++ src/org/group1f/izuna/GameCore.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.group1f.izuna;
+
+/**
+ *
+ * @author Mustafa
+ */
+public class GameCore {
+    public static void main(String args[]){
+        
+    }
+}
